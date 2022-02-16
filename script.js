359993e32181eabf938b264544f5573830468735
jess <jesicatorti@gmail.com>
2022-02-15 22:19:06 -0300

    improve2

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ var pokacho = new Pakiman("Pokacho", 80, 50);
 var tocinauro = new Pakiman ("Tocinauro", 120, 40);
 var marseno = new Pakiman ("Marseno", 70, 150);
 
-pokacho.mostrar(); 
-marseno.mostrar();
-cauchin.mostrar();
-tocinauro.mostrar();
+var coleccion = [];
+coleccion.push(new Pakiman("Cauchin", 100, 30));
+coleccion.push(new Pakiman("Pokacho", 80, 50));
+coleccion.push(new Pakiman ("Tocinauro", 120, 40));
+coleccion.push(new Pakiman ("Marseno", 70, 150));
+for (var pakimanes of coleccion){
+    pakimanes.mostrar();
+    }
