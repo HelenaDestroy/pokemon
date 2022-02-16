a00baab96ae475092ee89b22c503ff71b6eb15eb
jess <jesicatorti@gmail.com>
2022-02-15 21:35:53 -0300

    improve

--- script.js
+++ script.js
@@ -4,26 +4,6 @@ imagenes["Pokacho"] = "pollo.png";
 imagenes["Tocinauro"] = "cerdo.png";
 imagenes["Marseno"] = "cerdito.png";
 
-class Pakiman
-{
-    constructor(n,v,a)
-    {
-        this.imagen = new Image();
-        this.nombre = n;
-        this.vida = v;
-        this.ataque = a;
-        this.imagen.src= imagenes[this.nombre]
-        }
-    hablar (){alert(this.nombre)}   //LA INVOCO pokacho.hablar 
-    mostrar(){
-    document.body.appendChild(this.imagen);
-    document.write("<strong>" + this.nombre + "</strong><br/>"); 
-    document.write("Vida: " + this.vida + "<br/>"); 
-    document.write("Ataque: " + this.ataque + "<hr/>"); 
-}
-}
-
-
 var cauchin = new Pakiman("Cauchin", 100, 30);
 var pokacho = new Pakiman("Pokacho", 80, 50);
 var tocinauro = new Pakiman ("Tocinauro", 120, 40);
