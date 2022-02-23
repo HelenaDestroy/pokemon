d03220d897cce0d80cc18a051e39b5e53b4874aa
Jessica Torti <jesicatorti@gmail.com>
2022-02-23 10:12:51 -0300

    UNIFICANDO

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+class Pakiman
+{
+    constructor(n,v,a)
+    {
+       
+        this.nombre = n;
+        this.imagen = new Image();
+        this.vida = v;
+        this.ataque = a;
+        this.imagen.src= imagenes[this.nombre]
+        }
+    hablar (){alert(this.nombre)}   //LA INVOCO pokacho.hablar 
+    mostrar(){
+        
+        document.write("<strong>" + this.nombre + "</strong><br/>"); 
+        document.body.appendChild(this.imagen);
+        document.write("<br/>Vida: " + this.vida + "<br/>"); 
+    document.write("Ataque: " + this.ataque + "<hr/>"); 
+}
+}
+
 var imagenes = [];
 imagenes["Cauchin"] = "vaca.png";
 imagenes["Pokacho"] = "pollo.png";
